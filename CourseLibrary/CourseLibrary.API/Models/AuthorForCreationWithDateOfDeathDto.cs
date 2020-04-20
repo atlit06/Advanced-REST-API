88ca2865ae4467dd3b832268da14aac90dcd7c4c
atlit06 <atlithrastarson@gmail.com>
2020-04-20 15:53:28 +0000

    advanced content negotiation

---
+++ CourseLibrary/CourseLibrary.API/Models/AuthorForCreationWithDateOfDeathDto.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace CourseLibrary.API.Models
+{
+    public class AuthorForCreationWithDateOfDeathDto : AuthorForCreationDto
+    {
+        public DateTimeOffset? DateOfDeath { get; set; }
+    }
+}
